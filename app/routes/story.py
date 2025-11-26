2a55b4c80d4f2c7891e9a967d7eb65efc2eabe6f
adimidania <ja_adimi@esi.dz>
2025-11-26 20:13:04 +0100

    Commented out the images generation

--- app/routes/story.py
+++ app/routes/story.py
@@ -9,5 +9,5 @@ router = APIRouter()
 @router.post("/generate", response_model=GenerateStoryResponse)
 def generate_story_route(payload: GenerateStoryRequest):
     text = StoryGenerator().generate_story(prompt_vars=payload.dict())
-    images = StoryIllustrator().illustrate_story(story_text=text, output_prefix="storage/scene")
+    # images = StoryIllustrator().illustrate_story(story_text=text, output_prefix="storage/scene")
     return GenerateStoryResponse(text=text)
